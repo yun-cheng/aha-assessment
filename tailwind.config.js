58709c91fefd7814f4ac2a9222aaf3879dcf5d36
yun-cheng <yuncheng.dev@gmail.com>
2024-03-18 10:49:57 +0800

    chore(theme): configure default settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,25 @@ const formsPlugin = require('@tailwindcss/forms')
 
 /** @type {import('tailwindcss/types').Config} */
 const config = {
+	important: true,
 	content: ['index.html', 'src/**/*.tsx'],
 	theme: {
 		fontFamily: {
 			sans: ['Ubuntu', ...defaultConfig.theme.fontFamily.sans]
+		},
+		extend: {
+			colors: {
+				background: '#181818',
+				'near-black': '#121212'
+			},
+			screens: {
+				'2xl': '1440px'
+			},
+			letterSpacing: {
+				15: '.15px',
+				25: '.25px',
+				40: '.4px'
+			}
 		}
 	},
 	experimental: { optimizeUniversalDefaults: true },
