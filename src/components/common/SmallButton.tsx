7b22e264a3d630d17f9ce189753fc3d0f00c01d0
yun-cheng <yuncheng.dev@gmail.com>
2024-03-19 20:00:52 +0800

    feat(component): add common use SmallButton component

---
+++ src/components/common/SmallButton.tsx
@@ -0,0 +1,31 @@
+import type { ComponentProps, ReactElement } from 'react'
+import cn from 'utils/cn'
+
+type Props = ComponentProps<'button'> & {
+	variant?: 'contained' | 'outlined'
+}
+
+export default function SmallButton({
+	className,
+	children,
+	variant = 'contained',
+	...props
+}: Props): ReactElement {
+	return (
+		<button
+			className={cn(
+				'max-h-fit rounded-[20px] border border-white px-[10px] py-[5px]',
+				'font-["Open_Sans_Variable"] text-xs/normal font-semibold',
+				variant === 'contained'
+					? 'h-[28px] bg-white text-near-black hover:bg-near-black hover:text-white'
+					: 'h-[29px] bg-near-black text-white hover:bg-white hover:text-near-black',
+				className
+			)}
+			type='button'
+			// eslint-disable-next-line react/jsx-props-no-spreading
+			{...props}
+		>
+			{children}
+		</button>
+	)
+}
