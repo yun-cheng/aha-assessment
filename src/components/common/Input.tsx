bbacfd2d8cf2450b22ac5479a6b96116d49af7cc
yun-cheng <yuncheng.dev@gmail.com>
2024-03-21 10:15:28 +0800

    feat(component): add forwardRef to Input component

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,12 +1,15 @@
-import type { ComponentProps, ReactElement } from 'react'
+import type { ComponentProps, ForwardedRef, ReactElement } from 'react'
+import { forwardRef } from 'react'
+
 import cn from 'utils/cn'
 
-export default function Input({
-	className,
-	...props
-}: ComponentProps<'input'>): ReactElement {
+function Input(
+	{ className, ...props }: ComponentProps<'input'>,
+	ref: ForwardedRef<HTMLInputElement>
+): ReactElement {
 	return (
 		<input
+			ref={ref}
 			className={cn(
 				'h-[60px] w-full',
 				'rounded-md border-[3px] border-white/50 bg-transparent pl-[15px] focus:border-[#ff9b33] focus:ring-0',
@@ -18,3 +21,5 @@ export default function Input({
 		/>
 	)
 }
+
+export default forwardRef(Input)
