a844808688351f4b027413abfcf6d71e81264f8f
yun-cheng <yuncheng.dev@gmail.com>
2024-03-21 17:29:16 +0800

    feat(FollowSection): add functionality and infinite scrolling with useFetchFollowData and useInView

--- src/components/follow/FollowSection.tsx
+++ src/components/follow/FollowSection.tsx
@@ -1,25 +1,52 @@
 import Tabs from 'components/common/Tabs'
-import type { ReactElement } from 'react'
+import useFetchFollowData from 'hooks/useFetchFollowData'
+import type { ComponentProps, ReactElement } from 'react'
+import { useEffect, useRef } from 'react'
+
+import { useInView } from 'react-intersection-observer'
+import cn from 'utils/cn'
 import FollowUserItem from './FollowUserItem'
 
 export default function FollowSection(): ReactElement {
+	const containerRef = useRef<HTMLDivElement>(null)
+
+	const { ref, inView } = useInView({
+		root: containerRef.current,
+		rootMargin: '0px 0px 200px'
+	})
+
+	const {
+		type: tab,
+		setType: setTab,
+		data,
+		isFetching,
+		fetchNextPage
+	} = useFetchFollowData()
+
+	useEffect(() => {
+		if (inView) {
+			void fetchNextPage()
+		}
+	}, [fetchNextPage, inView])
+
+	const onChange: ComponentProps<typeof Tabs>['onChange'] = (_, value) => {
+		setTab(value as 'followers' | 'following')
+	}
+
 	return (
 		<div className='flex h-screen w-[375px] flex-col'>
-			<Tabs className='pt-[23px]' tabs={['Followers', 'Following']} />
-			<div className='w-full overflow-y-auto py-4 pl-4 pr-[9px]'>
-				{Array.from({ length: 30 }).map((_, index) => (
-					<FollowUserItem
-						// eslint-disable-next-line react/no-array-index-key
-						key={index}
-						user={{
-							id: index.toString(),
-							name: 'Fullname',
-							username: 'username',
-							avater: '',
-							isFollowing: Boolean(index % 2)
-						}}
-					/>
-				))}
+			<Tabs
+				className='pt-[23px]'
+				tabs={['followers', 'following']}
+				tab={tab}
+				onChange={onChange}
+			/>
+			<div
+				ref={containerRef}
+				className='w-full overflow-y-auto py-4 pl-4 pr-[9px]'
+			>
+				{data?.map(user => <FollowUserItem key={user.id} user={user} />)}
+				<div ref={ref} className={cn('w-full', { hidden: isFetching })} />
 			</div>
 		</div>
 	)
