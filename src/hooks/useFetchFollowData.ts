813b0efd1d2727ec5c7db5b8af480885aeb5053f
yun-cheng <yuncheng.dev@gmail.com>
2024-03-23 11:20:08 +0800

    feat(FollowSection): use avatars from faker

--- src/hooks/useFetchFollowData.ts
+++ src/hooks/useFetchFollowData.ts
@@ -1,14 +1,29 @@
+import { faker } from '@faker-js/faker'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import fetchFollowData from 'api/fetchFollowData'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 export default function useFetchFollowData() {
 	const [type, setType] = useState<'followers' | 'following'>('followers')
 
+	const fetchAndTransformFollowData = useCallback(
+		async ({ pageParam }: { pageParam: number }) => {
+			const typeNumber = type === 'followers' ? 0 : 1000
+			faker.seed(typeNumber + pageParam)
+			const followData = await fetchFollowData({ type, page: pageParam })
+			followData.data = followData.data.map(user => ({
+				...user,
+				avater: faker.image.avatarLegacy()
+			}))
+			return followData
+		},
+		[type]
+	)
+
 	const { status, data, error, isFetching, fetchNextPage, hasNextPage } =
 		useInfiniteQuery({
 			queryKey: ['fetchFollowData', type],
-			queryFn: async ({ pageParam: page }) => fetchFollowData({ type, page }),
+			queryFn: fetchAndTransformFollowData,
 			initialPageParam: 1,
 			getNextPageParam: lastPage =>
 				lastPage.page < lastPage.totalPages ? lastPage.page + 1 : undefined
