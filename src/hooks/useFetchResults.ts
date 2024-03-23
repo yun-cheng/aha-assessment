3639aaf94e14b30c36d6dcdd31bbdce3c067575a
yun-cheng <yuncheng.dev@gmail.com>
2024-03-23 14:13:35 +0800

    feat(ResultsPage): use images from faker

--- src/hooks/useFetchResults.ts
+++ src/hooks/useFetchResults.ts
@@ -1,5 +1,7 @@
+import { faker } from '@faker-js/faker'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import fetchResults from 'api/fetchResults'
+import { useCallback } from 'react'
 import { useSearchParams } from 'react-router-dom'
 
 export default function useFetchResults() {
@@ -8,11 +10,30 @@ export default function useFetchResults() {
 	const keyword = searchParams.get('keyword') ?? ''
 	const pageSize = Number(searchParams.get('pageSize'))
 
+	const fetchAndTransformResults = useCallback(
+		async ({ pageParam }: { pageParam: number }) => {
+			faker.seed(pageParam * 100 + pageSize)
+			const results = await fetchResults({
+				keyword,
+				pageSize,
+				page: pageParam
+			})
+			results.data = results.data.map(result => ({
+				...result,
+				avater: faker.image.urlPicsumPhotos({
+					width: 450,
+					height: 300
+				})
+			}))
+			return results
+		},
+		[keyword, pageSize]
+	)
+
 	const { status, data, error, isFetching, fetchNextPage, hasNextPage } =
 		useInfiniteQuery({
 			queryKey: ['fetchResults', keyword, pageSize],
-			queryFn: async ({ pageParam: page }) =>
-				fetchResults({ keyword, page, pageSize }),
+			queryFn: fetchAndTransformResults,
 			initialPageParam: 1,
 			getNextPageParam: lastPage =>
 				lastPage.page < lastPage.totalPages ? lastPage.page + 1 : undefined
